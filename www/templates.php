c0647685a13f56dae56aaabf57c102c216dd3875
Rich Hildred <rhildred@gmail.com>
2014-06-02 15:23:11 -0400

    finished working template

--- www/templates.php
+++ www/templates.php
@@ -1,11 +1,16 @@
 <?php
 
 $Viewbag = new stdClass();
+$Viewbag->bInLayout = false;
 
 function layout($sLayout){
-	global $Viewbag;
-	$bInLayout = true;
-	include_once $sLayout;
+	global $Viewbag, $bInLayout;
+	if(!$Viewbag->bInLayout){
+		$Viewbag->bInLayout = true;
+		include_once $sLayout;
+		//don't want to output anything from the layout the first time
+		exit;
+	}
 }
 
 function renderBody(){
